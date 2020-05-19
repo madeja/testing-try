fa1af6638fa4562314535b072184dfe6a0d3540b
Matej Madeja <info@madeja.sk>
2020-05-19 10:45:20 +0200

    test

--- halabala/HaluzTest.java
+++ halabala/HaluzTest.java
@@ -1,1 +1,24 @@
-HaluzTest.java
+test
+TestException
+MocnyTestException
+MocnyTest
+
+testexception
+mocnytestexception
+mocnytest
+
+test exception
+mocny test exception
+mocny test
+
+test_exception
+mocny_test_exception
+mocny_test
+
+test_exception.ext
+mocny_test_exception.ext
+mocny_test.ext
+
+pxt.test.ext
+pxt.test
+test.ext
